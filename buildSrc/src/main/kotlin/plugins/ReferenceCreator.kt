9d75daa70ca7ca37eab01a2b89a0c2f1145c7df2
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-11-17 23:24:22 +0100

    will this fix it?

--- buildSrc/src/main/kotlin/plugins/ReferenceCreator.kt
+++ buildSrc/src/main/kotlin/plugins/ReferenceCreator.kt
@@ -19,30 +19,28 @@ class ReferenceCreator : Plugin<Project> {
             val properties = Loader.loadPropertyFromFile(REFERENCE_FILE)
             val objectName = project.propertyString("mod_name").filter(Char::isLetterOrDigit)
             val objectPath = "${project.propertyString("root_package")}.${project.propertyString("mod_id")}"
-            val referenceContent = buildString {
-                appendLine("package $objectPath")
-                appendLine()
-                appendLine("internal typealias Reference = ${objectName}Reference")
-                appendLine()
-                appendLine("/**")
-                appendLine(" * Auto-generated reference object containing constants from `$REFERENCE_FILE`.")
-                appendLine(" * Don't change this file manually as it will be overwritten.")
-                appendLine(" */")
-                appendLine("@Suppress(\"UNUSED\")")
-                appendLine("object ${objectName}Reference {")
-                properties.forEach { (key, value) ->
-                    val eval = (if (value is String) project.evaluate(value) else value).toString()
-                    appendLine("    const val ${key.toString().uppercase()} = ${if (eval.all(Char::isDigit) && eval.count { it == '.' } <= 1) eval else "\"$eval\""}")
-                }
-                appendLine("}")
+            val reference = arrayListOf("package $objectPath")
+            reference.add("")
+            reference.add("internal typealias Reference = ${objectName}Reference")
+            reference.add("")
+            reference.add("/**")
+            reference.add(" * Auto-generated reference object containing constants from `$REFERENCE_FILE`.")
+            reference.add(" * Don't change this file manually as it will be overwritten.")
+            reference.add(" */")
+            reference.add("@Suppress(\"UNUSED\")")
+            reference.add("object ${objectName}Reference {")
+            properties.forEach { (key, value) ->
+                val eval = (if (value is String) project.evaluate(value) else value).toString()
+                reference.add("    const val ${key.toString().uppercase()} = ${if (eval.all(Char::isDigit) && eval.count { it == '.' } <= 1) eval else "\"$eval\""}")
             }
+            reference.add("}")
             val outputPath = "${project.rootDir}/src/main/kotlin/${project.propertyString("tags_package").replace(".", "/")}/${objectName}Reference.kt"
             val outputFile = Path(outputPath)
             if (outputFile.notExists()) {
                 outputFile.parent.createDirectories()
                 outputFile.createFile()
             }
-            outputFile.writeText(referenceContent)
+            outputFile.writeText(reference.joinToString(separator = System.lineSeparator()))
             Logger.info("Reference file created at ${outputPath.replace("\\", "/")}")
         }
     }
